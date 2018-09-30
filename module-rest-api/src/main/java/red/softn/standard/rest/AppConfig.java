3e286aa0f66371546381e01dd929590b6ea9919f
Nicolás Marulanda P <nicomaru@outlook.com>
2018-09-30 02:49:05 +0200

    Modulo Rest-API

---
+++ module-rest-api/src/main/java/red/softn/standard/rest/AppConfig.java
@@ -0,0 +1,7 @@
+package red.softn.standard.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class AppConfig extends Application {}
