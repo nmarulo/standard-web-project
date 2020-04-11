bbe098bfaacbe059557ccc1e68b9dab3b7af52a7
Nicolás Marulanda P <nicomaru@outlook.com>
2020-04-11 22:57:18 +0200

    Users - Agregado anotación "@QueryParam" en todas las propiedades de la clase. - Para el tipo Date se agrega además la anotación "@DateFormatterParamConverter", para que se pueda convertir la petición a fecha.

--- module-objects-api/src/main/java/red/softn/standard/objects/pojo/Users.java
+++ module-objects-api/src/main/java/red/softn/standard/objects/pojo/Users.java
@@ -3,28 +3,39 @@ package red.softn.standard.objects.pojo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
+import red.softn.standard.objects.DateFormatterParamConverter;
 import red.softn.standard.objects.MultiDateDeserializer;
 
+import javax.ws.rs.QueryParam;
 import java.util.Date;
 
 @Setter
 @Getter
 public class Users {
     
+    @QueryParam("id")
     private Integer id;
     
+    @QueryParam("userLogin")
     private String userLogin;
     
+    @QueryParam("userName")
     private String userName;
     
+    @QueryParam("userEmail")
     private String userEmail;
     
+    @QueryParam("userPassword")
     private String userPassword;
     
     @JsonDeserialize(using = MultiDateDeserializer.class)
+    @QueryParam("userRegistered")
+    @DateFormatterParamConverter
     private Date userRegistered;
     
+    @QueryParam("profileId")
     private Integer profileId;
     
+    @QueryParam("userUrlImage")
     private String userUrlImage;
 }
