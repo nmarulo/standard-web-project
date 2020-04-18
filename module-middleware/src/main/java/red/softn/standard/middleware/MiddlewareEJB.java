edd302c66a16753616164ecbe3a419b978a9c65b
Nicolás Marulanda P <nicomaru@outlook.com>
2020-04-18 14:07:34 +0200

    Middleware - [Corrección] Inicializar y cerrar conexión. Estaba cerrando la instancia de la factory provocando un error al crear otra conexión.

--- module-middleware/src/main/java/red/softn/standard/middleware/MiddlewareEJB.java
+++ module-middleware/src/main/java/red/softn/standard/middleware/MiddlewareEJB.java
@@ -1,7 +1,10 @@
 package red.softn.standard.middleware;
 
+import red.softn.standard.db.common.DbEJB;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceUnit;
 
 public abstract class MiddlewareEJB {
@@ -9,15 +12,20 @@ public abstract class MiddlewareEJB {
     @PersistenceUnit(unitName = "SoftnPU")
     private EntityManagerFactory entityManagerFactory;
     
+    EntityManager entityManager;
+    
     protected EntityManager getConnection() {
-        return entityManagerFactory.createEntityManager();
+        this.entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = this.entityManager.getTransaction();
+        transaction.begin();
+        
+        return this.entityManager;
     }
     
     protected void closeConnection() {
-        if (this.entityManagerFactory != null && this.entityManagerFactory.isOpen()) {
-            this.entityManagerFactory.close();
+        if (this.entityManager != null && this.entityManager.isOpen()) {
+            this.entityManager.close();
         }
     }
-    
     
 }
