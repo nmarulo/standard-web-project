7e7a583f53926b969e8208023883e9173b0aac95
Nicolás Marulanda P <nicomaru@outlook.com>
2020-04-18 13:05:47 +0200

    Middleware - Cierre de conexiones. - Renombrado "AMiddleware" => "MiddlewareEJB"

--- module-middleware/src/main/java/red/softn/standard/middleware/MiddlewareEJB.java
+++ module-middleware/src/main/java/red/softn/standard/middleware/MiddlewareEJB.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
-public abstract class AMiddleware {
+public abstract class MiddlewareEJB {
     
     @PersistenceUnit(unitName = "SoftnPU")
     private EntityManagerFactory entityManagerFactory;
@@ -12,4 +12,12 @@ public abstract class AMiddleware {
     protected EntityManager getConnection() {
         return entityManagerFactory.createEntityManager();
     }
+    
+    protected void closeConnection() {
+        if (this.entityManagerFactory != null && this.entityManagerFactory.isOpen()) {
+            this.entityManagerFactory.close();
+        }
+    }
+    
+    
 }
--- module-middleware/src/main/java/red/softn/standard/middleware/ejb/UsersM.java
+++ module-middleware/src/main/java/red/softn/standard/middleware/ejb/UsersM.java
@@ -3,7 +3,7 @@ package red.softn.standard.middleware.ejb;
 import red.softn.standard.common.GsonUtil;
 import red.softn.standard.db.api.UsersDI;
 import red.softn.standard.db.dto.UsersDTO;
-import red.softn.standard.middleware.AMiddleware;
+import red.softn.standard.middleware.MiddlewareEJB;
 import red.softn.standard.middleware.api.UsersMI;
 import red.softn.standard.objects.ARequest;
 import red.softn.standard.objects.request.UserRequest;
@@ -14,50 +14,74 @@ import javax.inject.Inject;
 import java.util.List;
 
 @Stateless
-public class UsersM extends AMiddleware implements UsersMI {
+public class UsersM extends MiddlewareEJB implements UsersMI {
     
     @Inject
     private UsersDI usersDI;
     
     @Override
     public List<UserResponse> get(ARequest<UserRequest> aRequest) throws Exception {
-        UserRequest    request = aRequest.getRequest();
-        List<UsersDTO> dtoList = this.usersDI.find(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
-        
-        return GsonUtil.convertObjectListTo(dtoList, UserResponse[].class);
+        try {
+            UserRequest    request = aRequest.getRequest();
+            List<UsersDTO> dtoList = this.usersDI.find(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
+    
+            return GsonUtil.convertObjectListTo(dtoList, UserResponse[].class);
+        }finally {
+            closeConnection();
+        }
     }
     
     @Override
     public UserResponse getById(ARequest<Integer> aRequest) throws Exception {
-        UsersDTO dto = this.usersDI.findById(getConnection(), aRequest.getRequest());
-        
-        return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        try {
+            UsersDTO dto = this.usersDI.findById(getConnection(), aRequest.getRequest());
+    
+            return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        }finally {
+            closeConnection();
+        }
     }
     
     @Override
     public UserResponse post(ARequest<UserRequest> aRequest) throws Exception {
-        UserRequest request = aRequest.getRequest();
-        UsersDTO    dto     = this.usersDI.insert(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
-        
-        return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        try {
+            UserRequest request = aRequest.getRequest();
+            UsersDTO    dto     = this.usersDI.insert(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
+    
+            return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        }finally {
+            closeConnection();
+        }
     }
     
     @Override
     public UserResponse put(ARequest<UserRequest> aRequest) throws Exception {
-        UserRequest request = aRequest.getRequest();
-        UsersDTO    dto     = this.usersDI.update(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
-        
-        return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        try {
+            UserRequest request = aRequest.getRequest();
+            UsersDTO    dto     = this.usersDI.update(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
+    
+            return GsonUtil.convertObjectTo(dto, UserResponse.class);
+        }finally {
+            closeConnection();
+        }
     }
     
     @Override
     public void delete(ARequest<UserRequest> aRequest) throws Exception {
-        UserRequest request = aRequest.getRequest();
-        this.usersDI.delete(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
+        try {
+            UserRequest request = aRequest.getRequest();
+            this.usersDI.delete(getConnection(), GsonUtil.convertObjectTo(request, UsersDTO.class));
+        }finally {
+            closeConnection();
+        }
     }
     
     @Override
     public void deleteById(ARequest<Integer> aRequest) throws Exception {
-        this.usersDI.deleteById(getConnection(), aRequest.getRequest());
+        try {
+            this.usersDI.deleteById(getConnection(), aRequest.getRequest());
+        }finally {
+            closeConnection();
+        }
     }
 }
