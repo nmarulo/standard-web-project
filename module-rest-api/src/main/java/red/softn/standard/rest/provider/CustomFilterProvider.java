1a3572ae28b652259c0e533ad4f885c7590ae061
Nicolás Marulanda P <nicomaru@outlook.com>
2020-08-09 22:06:07 +0200

    CustomFilterProvider - Se reemplaza el uso de "CustomHttpException" por "BadRequestException". - Ahora al filtrar la respuesta, se comprueba que el código de estado http este entre 200 y 299, para no sobre escribir los códigos de estado de las excepciones.

--- module-rest-api/src/main/java/red/softn/standard/rest/provider/CustomFilterProvider.java
+++ module-rest-api/src/main/java/red/softn/standard/rest/provider/CustomFilterProvider.java
@@ -1,13 +1,10 @@
 package red.softn.standard.rest.provider;
 
-import org.apache.commons.lang3.StringUtils;
-import red.softn.standard.common.rest.CustomHttpException;
-import red.softn.standard.rest.ExceptionMapperProvider;
 import red.softn.standard.rest.annotation.CustomRestApi;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.*;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.function.Consumer;
@@ -26,9 +23,9 @@ public class CustomFilterProvider implements ContainerRequestFilter, ContainerRe
     
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-        String headerString = containerResponseContext.getHeaderString(ExceptionMapperProvider.EXCEPTION_HEADER_NAME);
+        int status = containerResponseContext.getStatus();
         
-        if (StringUtils.isBlank(headerString)) {
+        if (status >= 200 && status < 300) {
             checkAnnotations(CustomRestApi::statusResponse, containerResponseContext::setStatusInfo);
         }
     }
@@ -39,7 +36,7 @@ public class CustomFilterProvider implements ContainerRequestFilter, ContainerRe
     
     private void checkBody(boolean canNullRequest, ContainerRequestContext containerRequestContext) {
         if (containerRequestContext.getLength() <= 0 && !canNullRequest) {
-            throw new CustomHttpException("El cuerpo de la petición no puede estar vació.", Response.Status.BAD_REQUEST);
+            throw new BadRequestException("El cuerpo de la petición no puede estar vació.");
         }
     }
     
