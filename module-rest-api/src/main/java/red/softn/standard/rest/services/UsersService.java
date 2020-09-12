ae17d896c9893bc81ad46046b9555713e7a44782
Nicolás Marulanda P <nicomaru@outlook.com>
2020-09-12 02:11:24 +0200

    Modulo Rest-Api - Se deja de usar la clase "ARestService". - Se actualiza la implementación de las peticiones para hacer uso de la anotación "CustomRestApi", se reemplaza el tipo de respuesta "Response" por el correspondiente a cada método

--- module-rest-api/src/main/java/red/softn/standard/rest/services/UsersService.java
+++ module-rest-api/src/main/java/red/softn/standard/rest/services/UsersService.java
@@ -2,56 +2,62 @@ package red.softn.standard.rest.services;
 
 import red.softn.standard.middleware.api.UsersMI;
 import red.softn.standard.objects.request.UserRequest;
-import red.softn.standard.rest.ARestService;
+import red.softn.standard.objects.response.UserFormCreateUpdateResponse;
+import red.softn.standard.objects.response.UserResponse;
+import red.softn.standard.rest.annotation.CustomRestApi;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class UsersService extends ARestService {
+public class UsersService {
     
     @Inject
     private UsersMI usersMI;
     
     @GET
-    public Response get(@BeanParam UserRequest request) {
-        return getResponse(request, this.usersMI::get, true);
+    public List<UserResponse> search(@BeanParam UserRequest request) throws Exception {
+        return this.usersMI.search(request);
     }
     
     @GET
     @Path("{id}")
-    public Response getById(@PathParam("id") Integer id) {
-        return getResponse(id, this.usersMI::getById);
+    public UserResponse getById(@PathParam("id") Integer request) throws Exception {
+        return this.usersMI.getById(request);
     }
     
     @POST
-    public Response post(UserRequest request) {
-        return getResponse(request, this.usersMI::post);
+    @CustomRestApi(statusResponse = Response.Status.CREATED)
+    public UserResponse post(UserRequest request) throws Exception {
+        return this.usersMI.post(request);
     }
     
     @PUT
-    public Response put(UserRequest request) {
-        return getResponse(request, this.usersMI::put);
+    public UserResponse put(UserRequest request) throws Exception {
+        return this.usersMI.put(request);
     }
     
     @DELETE
     @Path("{id}")
-    public Response deleteById(@PathParam("id") Integer id) {
-        return getResponse(id, this.usersMI::deleteById);
+    @CustomRestApi(statusResponse = Response.Status.NO_CONTENT)
+    public void deleteById(@PathParam("id") Integer request) throws Exception {
+        this.usersMI.deleteById(request);
     }
     
     @GET
     @Path("form-create-update")
-    public Response formCreateUpdate() {
+    public UserFormCreateUpdateResponse formCreateUpdate() throws Exception {
         return formCreateUpdate(null);
     }
     
     @GET
     @Path("form-create-update/{id}")
-    public Response formCreateUpdate(@PathParam("id") Integer id) {
-        return getResponse(id, this.usersMI::formCreateUpdate, true);
+    public UserFormCreateUpdateResponse formCreateUpdate(@PathParam("id") Integer request) throws Exception {
+        return this.usersMI.formCreateUpdate(request);
     }
 }
